6f7cc99646cac5860c125dd32b84c8c75ef30db3
Dakoda Armstrong <mtlca66@gmail.com>
2022-01-10 21:55:06 -0700

    added case insensitve code to playerselection parameter. tested successfully via console log.

--- script.js
+++ script.js
@@ -11,23 +11,23 @@ function computerPlay() {
 //Play a round between player selection vs computer selection
 
   function playRound(playerSelection, computerSelection) {
-    if (playerSelection === 'rock' && computerSelection === 'paper') {
+    if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper') {
         return "You lose! Paper beats rock";
-    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+    } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors') {
         return "You win! Rock beats scissors";
-    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+    } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock') {
         return "You win! Paper beats rock";
-    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+    } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissors') {
         return "You lose! Scissors beats paper";
-    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+    } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper') {
         return "You win! Scissors beats paper";
-    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+    } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'rock') {
         return "You lose! Rock beats scissors";
     } else 
         return "Draw"
     }
    
-const playerSelection = 'scissors';
+const playerSelection = 'rock';
 const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
 
