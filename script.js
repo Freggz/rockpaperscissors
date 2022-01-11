03d7f4cbd4edf7e702932ccc5347401c2c1ad2e2
Dakoda Armstrong <mtlca66@gmail.com>
2022-01-10 22:44:28 -0700

    added prompt for player

--- script.js
+++ script.js
@@ -12,22 +12,22 @@ function computerPlay() {
 
   function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper') {
-        return "You lose! Paper beats rock";
+        return "You lose! Paper beats rock.";
     } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors') {
-        return "You win! Rock beats scissors";
+        return "You win! Rock beats scissors.";
     } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock') {
-        return "You win! Paper beats rock";
+        return "You win! Paper beats rock.";
     } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissors') {
-        return "You lose! Scissors beats paper";
+        return "You lose! Scissors beats paper.";
     } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper') {
-        return "You win! Scissors beats paper";
+        return "You win! Scissors beats paper.";
     } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'rock') {
-        return "You lose! Rock beats scissors";
+        return "You lose! Rock beats scissors.";
     } else 
-        return "Draw"
+        return "Tie."
     }
    
-const playerSelection = 'rock';
+const playerSelection = prompt("Let's play rock, paper, scissors. GO!");
 const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
 
