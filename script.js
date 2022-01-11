ac2fe9ee5de3748cd65f04b442c73af1f8460c31
Dakoda Armstrong <mtlca66@gmail.com>
2022-01-10 20:11:02 -0700

    created computerplay function that randomly selects rock/paper/scissors

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 //Create function for computer to randomly select rock, paper or scissors
+const choices = ['rock','paper','scissors'];
+
+function computerPlay(choices) {
+  return choices[Math.floor(Math.random() * choices.length)]
+}
+
+//console.log(computerPlay(choices)) - it works!
+
+
+
+
+
+
+
+
+
 //Create function for player to input rock, paper or scissors
 //Play a round between player selection vs computer selection
 //Create function to play 5 rounds and declare a winner
