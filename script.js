42a4b0742e7643e395c7cffdc4eab776ba224921
Dakoda Armstrong <mtlca66@gmail.com>
2022-01-10 19:56:29 -0700

    added pseudocode to script file for planning

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-console.log("Hello World");
+//Create function for computer to randomly select rock, paper or scissors
+//Create function for player to input rock, paper or scissors
+//Play a round between player selection vs computer selection
+//Create function to play 5 rounds and declare a winner
